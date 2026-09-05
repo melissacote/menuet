7b0d266a36b70160c7d568a831e2696d37743229
Melissa Cote <diamondmeadows22@gmail.com>
2026-09-05 19:58:42 -0400

    -update import statements in test files

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,5 @@
 from sqlalchemy import Connection, text
 
-from menuet.db import get_connection
-
-
 def test_db_fk(conn: Connection) -> None:
     """Assert that foreign key enforcement is on."""
     result = conn.execute(text("PRAGMA foreign_keys")).fetchone()
